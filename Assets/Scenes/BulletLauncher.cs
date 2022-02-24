a721a333e8901505d65dbbf2b1b7dc3bffc80bb4
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-02-24 17:01:00 +0530

    Getting the bullet direction according to the camera forward direction

--- Assets/Scenes/BulletLauncher.cs
+++ Assets/Scenes/BulletLauncher.cs
@@ -19,9 +19,12 @@ public class BulletLauncher : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject tempBullet=Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            GameObject tempBullet=Instantiate(bulletPrefab, transform.position,Quaternion.identity);
+ 
             rb = tempBullet.GetComponent<Rigidbody>();
-            rb.velocity=(Vector3.forward * speed);
+            tempBullet.transform.position = transform.position;
+           // Camera camera = GetComponentInChildren<Camera>();
+            rb.velocity=Camera.main.transform.rotation* (Vector3.forward * speed);
             
         }
 
