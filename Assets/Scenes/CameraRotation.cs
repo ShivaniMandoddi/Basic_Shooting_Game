1ec018f7eea4d48a801d96d64e9f884cabd19a76
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-02-24 16:25:30 +0530

    Camera is fixed

--- Assets/Scenes/CameraRotation.cs
+++ Assets/Scenes/CameraRotation.cs
@@ -8,18 +8,17 @@ public class CameraRotation : MonoBehaviour
     public float rotationSpeed;
     void Start()
     {
-        
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-             float mouseX = Input.GetAxis("Mouse X");
+        float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
-        transform.rotation =transform.rotation * Quaternion.Euler(-mouseY*rotationSpeed,mouseX*rotationSpeed, 0);
-        
-
+        transform.rotation = transform.rotation * Quaternion.Euler(0, mouseX * rotationSpeed, 0);
+        Camera cam = GetComponentInChildren<Camera>();
+        cam.transform.localRotation = Quaternion.Euler(-mouseY, 0, 0) * cam.transform.localRotation;
     }
-    
 }
